1864a3130ab225c19c1bdc356e81756e09618d59
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-12 20:46:41 +0300

    feat: add BookingController with GET and POST endpoints

---
+++ src/main/java/com/hotelbookingapi/hotel_booking_api/controller/BookingController.java
@@ -0,0 +1,37 @@
+package com.hotelbookingapi.hotel_booking_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping
+    public List<Booking> getBookings() {
+        return bookingService.getBookings();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addBooking(@RequestBody Booking booking) {
+
+        try {
+            Booking newBooking = bookingService.addBooking(booking);
+            return ResponseEntity.ok(newBooking);
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(409).body(e.getMessage());
+        }
+    }
+}
